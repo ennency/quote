4986aedb0c4efe2a1e62f1a70a2b5a47a40db353
Carlo <carlo.tagnesi@gmail.com>
2021-05-11 13:08:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <header>
+          <RandomButton />
+          <QuoteAuthor />
+        </header>
+        <Quote />
+        <button type="button">
+          <h3>Bill gates</h3>
+          <div>Business</div>
+        </button>
+        <footer>Created by ennency - devChallenges.io</footer>
+      </div>
+    </main>
   );
 }
 
+export const Quote = (quote) => {
+  return (
+    <div>
+      {quote}
+    </div>
+  )
+}
+
+export const RandomButton = () => <button type="button">Prova</button>;
+
+export const QuoteAuthor = (name) => <h1>{name}</h1>;
 export default App;
